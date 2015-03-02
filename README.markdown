015f13382a69351610c399ed823ec9faa82087c6
mfidino <mfidino@lpzoo.org>
2015-03-01 21:37:29 -0600

    initial commit

---
+++ README.markdown
@@ -0,0 +1,1 @@
+A ruby file that makes it possible to parse through camera trap photos
---
+++ parse_photo_exif.rb
@@ -0,0 +1,27 @@
+require 'bundler/setup'
+Bundler.require(:default)
+Dotenv.load
+
+photo_paths_log = 'photo_paths.log'
+
+if File.file?(photo_paths_log)
+  photo_list = File.open(photo_paths_log, 'r').split("\n")
+else 
+  photo_list = Dir["#{ENV['PHOTO_PATH']}/**/*.JPG"]
+end
+
+tsv = "output_#{ENV['PHOTO_PATH'].split('/').last}.txt"
+
+unless File.file?(tsv) 
+  File.write(tsv, "path\tdate_time\tsite\tname")
+end 
+
+begin 
+  photo_path = photo_list.pop
+  #File.write(photo_paths_log, photo_list.join("\n"))
+  exif = EXIFR::JPEG.new(photo_path)
+  image_name = photo_path.split('/').last
+  tsv_line = "#{photo_path}\t#{exif.date_time}\t#{image_name[/.*\s/].gsub(/^[^\s]*$/,"")}\t#{image_name[/\(\d*\)/].gsub(/\D/,"")}"
+  File.open(tsv, 'a') { |f| f.puts tsv_line}
+end while photo_list.count > 0
+
