2617b91062379711d360a85be2ed3a01a435f509
mfidino <mfidino@lpzoo.org>
2015-04-09 10:27:29 -0500

    fixed issue when errors occur

--- parse_photo_exif.rb
+++ parse_photo_exif.rb
@@ -18,14 +18,15 @@ end
 
 begin 
   photo_path = photo_list.pop
+
   #File.write(photo_paths_log, photo_list.join("\n"))
   exif = EXIFR::JPEG.new(photo_path)
-rescue EXIFR::MalformedJPEG
+rescue 
 	tsv_line = "#{photo_path}\tNA\tNA\tNA\tNA"
-	File.open(tsv, 'a') { |f| f.puts tsv_line}
+	File.open(tsv, 'a') { |f| f.puts tsv_line} 
 else
   image_name = photo_path.split('/').last
-  tsv_line = "#{photo_path}\t#{exif.date_time}\t#{image_name[/^[^ ]*/]}\t#{image_name[/^\w+-\w+/]}\t#{image_name[/\(\d*\)/].gsub(/\D/,"")}"
+  tsv_line = "#{photo_path}\t#{exif.date_time}\t#{image_name[/^[^ ]*/]}\t#{image_name[/^\w+-\w+/]}\t#{image_name[/\(\d+\)/].gsub(/\D/,"")}"
   File.open(tsv, 'a') { |f| f.puts tsv_line}
 end while photo_list.count > 0
 
